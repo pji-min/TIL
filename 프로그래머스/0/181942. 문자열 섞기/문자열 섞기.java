a1fca5f19ac93d489b7e198b1d930c84075c790b
Meowwww- <162944522+pji-min@users.noreply.github.com>
2024-10-31 08:26:02 +0900

    [level 0] Title: 문자열 섞기, Time: 0.05 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String str1, String str2) {
+
+        StringBuilder answer = new StringBuilder();
+       for(int i = 0; i < str1.length(); i++) {
+           answer.append(str1.charAt(i));
+           answer.append(str2.charAt(i));
+        }
+        String result = answer.toString();
+            return result;
+
+    }
+}
